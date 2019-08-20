0d9f8e4d39816d12925cff1b1884542461b2ea2c
jakub41 <lemiszewski@gmx.com>
2019-08-21 00:57:29 +0200

    Add stuff to array example

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const KoaRoutter = require('koa-router');
 const json = require('koa-json');
 const path = require('path');
 const render = require('koa-ejs');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new KoaRoutter();
 
 // A Global To Simulate Data From DB -> Replace with DB
-const stuff = ['My Family', 'Sport', 'Music']; 
+const stuff = ['My Family', 'Sport', 'Music'];
 
 // Json Prettier Middleware
 app.use(json());
+
+// BodyParser Middleware
+app.use(bodyParser());
 
 // Simple Middleware Example
 //app.use(async ctx => ctx.body = { msg:'Hello World' });
@@ -25,16 +29,31 @@ render(app, {
     debug: false
 });
 
-// Index
-router.get('/', async ctx => {
+// Routes
+router.get('/', index);
+router.get('/add', showAdd);
+router.post('/add', add);
+router.get('/test', ctx => (ctx.body = 'hello test'));
+
+// List Of Stuff
+async function index(ctx) {
     await ctx.render('index', {
         title: 'Koa new stuff',
         stuff: stuff
     });
-});
+}
 
-// Routes
-router.get('/test', ctx => (ctx.body = 'hello test'));
+// Show Add Page
+async function showAdd(ctx) {
+    await ctx.render('add');
+}
+
+// Add Stuff
+async function add(ctx) {
+    const body = ctx.request.body;
+    stuff.push(body.stuff);
+    ctx.redirect("/"); 
+}
 
 // Router Middleware
 app.use(router.routes()).use(router.allowedMethods());
